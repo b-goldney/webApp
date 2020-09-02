d6637e977d35bd313660179effe3391805ddab5e
b-goldney <BrandonGoldney@gmail.com>
2020-09-01 19:52:50 -0500

    Created secret_settings

--- stocks/settings.py
+++ stocks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -19,8 +20,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&-@&i3m)@p^y1p04a4y7w$2ybb$h&=2wpfqnpi(lm($p%=%9fa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
